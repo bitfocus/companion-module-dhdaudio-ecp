aeb219618a33899b8e1df4aa9554f392f24cda62
Daniel Lippens <daniellippens@gmail.com>
2025-11-27 22:19:45 +0100

    fix: add postbuild script to include @companion-module/base/package.json

---
+++ scripts/postbuild.mjs
@@ -0,0 +1,18 @@
+/**
+ * Post-build script to add @companion-module/base/package.json to the package
+ * This is required for Companion to verify the API version
+ */
+import { mkdirSync, copyFileSync, existsSync } from 'fs'
+import { dirname } from 'path'
+
+const targetDir = 'pkg/node_modules/@companion-module/base'
+const sourceFile = 'node_modules/@companion-module/base/package.json'
+const targetFile = `${targetDir}/package.json`
+
+// Create directory structure
+mkdirSync(targetDir, { recursive: true })
+
+// Copy the package.json
+copyFileSync(sourceFile, targetFile)
+
+console.log('Added @companion-module/base/package.json to package')
